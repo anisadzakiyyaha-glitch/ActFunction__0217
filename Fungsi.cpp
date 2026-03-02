a7e6cbf224ccebe86023ce3637fad09973482a4d
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-02 22:15:46 +0700

    inisialisasi project

--- Fungsi.cpp
+++ Fungsi.cpp
@@ -4,19 +4,3 @@ using namespace std;
 //deklarasi variabel
 int p, l;
 //implementasi fungsi dan prosedur
-void input (){
-    cout << "masukkan panjang : ";
-    cin >> p;
-    cout << "masukkan lebar : ";
-    cin >> l;
-}
-int Luaspersegi(){
-    return p * l;
-}
-void output(){
-    cout << "Hasilnya : " << Luaspersegi();
-}
-int main(){ //mulai
-    input();
-    output();
-}//selesai
